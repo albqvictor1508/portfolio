d76a7c7dd7be62fb84d0ab5e18e56c136671f69d
albqvictor1508 <albq.victor@gmail.com>
2025-07-12 15:34:22 -0300

    chore: starting the api

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -19,22 +19,10 @@ type Technology struct {
 	Name string `json:"name"`
 }
 
-func returnTwoTimes(number1 int, number2 int) (int, int) {
-	var error error
-	if(number2 == 0) {
-		error = errors.New("Cannot divide by zero")
-		return 0,0,error
-	}
-	return number1 / number2, number1 % number2, error
-}
-
 func main() {
 	const salve string = "salve"
 	fmt.Println("Server Running on 3333!")
 
 	var result, remainder, error int = returnTwoTimes(10, 2)
-	if(error != nil) {
-		fmt.Println(error.Error())
-	}
 	fmt.Printf("The result: %v, and the remainder: %v", result, remainder)
 }
