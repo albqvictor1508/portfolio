9487149fce412567185a3063af989e8e68830c92
albqvictor1508 <albq.victor@gmail.com>
2025-07-12 14:25:06 -0300

    wip: baby steps in golang

--- server/src/server.go
+++ server/src/server.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"net/http"
+	"time"
 )
+/*
+var role {} = {
+	BACK: "back-end"
+	FRONT: "front-end"
+}
+*/
 
 type Project struct {
-	ID long `json:"id"` 
+	ID int64 `json:"id"` 
 	GithubUrl string `json:"github_url"`
-	//CreatedAt date `json:"created_at"`
+	DemoUrl string `json:"demo_url"`
+	Description string `json:"description"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Technology struct {
+	ID int64 `json:"id"`
+	ProjectId long `json:"project_id"`
 }
 
 func main() {
 	server := "fake server my guy"
 	fmt.Println(server)
+
+	log.Fatal(http.ListenAndServe(":3333", nil))
 }
