f72830a6b453cc06548d6885edebe3c660788696
albqvictor1508 <albq.victor@gmail.com>
2025-07-12 15:20:13 -0300

    wip: learning go basics

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -18,6 +18,12 @@ type Technology struct {
 	Name string `json:"name"`
 }
 
+func returnTwoTimes(number1 int, number2 int) (int, int) {
+	return number1 / number2, number1 % number2
+}
+
+var result, remainder int = returnTwoTimes(10, 2)
+
 func main() {
 	const salve string = "salve"
 	fmt.Println("Server Running on 3333!")
