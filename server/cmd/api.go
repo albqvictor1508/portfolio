528b799344f2a5a4fa3496835cca1a132c94f24c
albqvictor1508 <albq.victor@gmail.com>
2025-07-13 18:24:13 -0300

    chore: changes on api.go

--- server/cmd/api.go
+++ server/cmd/api.go
@@ -4,8 +4,6 @@ import (
 	"time"
 )
 
-// Project representa a estrutura de um projeto no banco de dados.
-// As tags `json:"..."` definem como cada campo será representado no JSON da API.
 type Project struct {
 	ID          int64     `json:"id"`
 	CategoryID  int64     `json:"categoryId"`
@@ -17,7 +15,6 @@ type Project struct {
 	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
-// Technology representa uma tecnologia que pode ser associada a um projeto.
 type Technology struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
@@ -25,22 +22,19 @@ type Technology struct {
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
-// ProjectTechnology é a tabela de associação entre um Projeto e uma Tecnologia.
-type ProjectTechnology struct {
+type ProjectTechnologies struct {
 	ID           int64     `json:"id"`
 	ProjectID    int64     `json:"projectId"`
-	TechnologyID int64     `json:"technologyId"` // Adicionado para relacionar com a tecnologia
+	TechnologyID int64     `json:"technologyId"`
 	CreatedAt    time.Time `json:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
 }
 
-// Category representa a categoria de um projeto.
 type Category struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
-// Error representa uma estrutura padrão para erros da API.
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
