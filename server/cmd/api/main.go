9d313766938aaba2d9091b4d9aa63377ef8006fd
albqvictor1508 <albq.victor@gmail.com>
2025-07-13 15:11:52 -0300

    chore: sql to create tables created

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Olá, Mundo!"))
 	})
--- server/migrations/creating-tables_2025-07-13.sql
+++ server/migrations/creating-tables_2025-07-13.sql
@@ -1,9 +1,24 @@
 CREATE TABLE projects(
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT, -- poderia ser um CUID, pra eu colocar na URL sem perigo do usuário pular de projeto
+  category_id INT FOREIGN KEY(category_id) REFERENCES categories(id),
   name VARCHAR(50) UNIQUE NOT NULL,
   description VARCHAR(255) NOT NULL,
   github_url VARCHAR(255) NOT NULL,
   demo_url VARCHAR(255),
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   updated_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE technologies(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  project_id INT FOREIGN KEY(project_id) REFERENCES projects(id)
+  category_id INT FOREIGN KEY(category_id) REFERENCES categories(id),
+  name VARCHAR(50) NOT NULL
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE categories(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) UNIQUE NOT NULL
 );
