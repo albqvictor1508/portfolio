15634d8f8ff529650540e2a23928a48293f532ae
albqvictor1508 <albq.victor@gmail.com>
2025-07-27 19:26:08 -0300

    chore: changes on golang api

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	_, err := db.NewConnection(databaseUrl)
+	if err != nil {
+		log.Fatal("ERROR TO CONNECT TO DATABASE: ", err)
+	}
 	g := gin.Default()
+
 	g.GET("/", func(context *gin.Context) {
 		context.JSON(200, gin.H{ // gin.H é uma expressão que manda response, n sei pq
 			"message": "Hello, World",
 		})
 	})
 
-	g.Run(":3333")
+	if err := g.Run(":3333"); err != nil {
+		log.Fatal("Fail to initialize server: ", err)
+	}
 }
