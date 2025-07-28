036fedd4581c622c535cfbc5e96b5ece04f2e7e9
albqvictor1508 <albq.victor@gmail.com>
2025-07-27 22:21:45 -0300

    chore: insert func

--- server/internal/project/repository.go
+++ server/internal/project/repository.go
@@ -12,14 +12,20 @@ type Repository struct {
 	Conn *pgxpool.Conn
 }
 
-func (r *Repository) Insert(project internal.Project) error {
+func (r *Repository) Insert(project internal.Project) (internal.Project, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	_, err := r.Conn.Exec(
+	err := r.Conn.QueryRow(
 		ctx,
-		"INSERT INTO projects () VALUES()",
-	)
+		"INSERT INTO projects (github_url, demo_url, is_pinned) VALUES($1, $2, $3) RETURNING id, github_url, demo_url, is_pinned",
+		project.GithubURL,
+		project.DemoURL,
+		project.IsPinned,
+	).Scan(&project.ID, &project.GithubURL, &project.DemoURL, &project.IsPinned)
+	if err != nil {
+		return internal.Project{}, err
+	}
 
-	return err
+	return project, nil
 }
