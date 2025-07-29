43a54fc52ee6d122191423d78e79abe83174b911
albqvictor1508 <albq.victor@gmail.com>
2025-07-29 10:58:55 -0300

    chore: category func and category route

--- server/function/category.go
+++ server/function/category.go
@@ -1,16 +1,20 @@
 package function
 
-import "github.com/albqvictor1508/portfolio/repository"
+import (
+	"github.com/albqvictor1508/portfolio/entity"
+	"github.com/albqvictor1508/portfolio/repository"
+)
 
 type CategoryFunc struct {
 	repo repository.CategoryRepository
 }
 
-func NewCategoryFunc(repo *repository.CategoryRepository) CategoryFunc {
+func NewCategoryFunc(repo repository.CategoryRepository) CategoryFunc {
 	return CategoryFunc{
-		repo: *repo,
+		repo: repo,
 	}
 }
 
-func (cf *CategoryFunc) CreateCategory() {
+func (cf *CategoryFunc) CreateCategory(category *entity.Category) (int, error) {
+	return cf.repo.Insert(category)
 }
---
+++ server/routes/category.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/albqvictor1508/portfolio/entity"
+	"github.com/albqvictor1508/portfolio/function"
+	"github.com/gin-gonic/gin"
+)
+
+type CategoryRoutes struct {
+	categoryFunc function.CategoryFunc
+}
+
+func NewCategoryRoute(categoryFunc function.CategoryFunc) CategoryRoutes {
+	return CategoryRoutes{
+		categoryFunc: categoryFunc,
+	}
+}
+
+func (cr *CategoryRoutes) CreateCategory(ctx *gin.Context, category *entity.Category) {
+	id, err := cr.categoryFunc.CreateCategory(category)
+	if err != nil {
+		errorMessage := fmt.Sprintf("ERROR ON CREATE CATEGORY: %v", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": errorMessage,
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"category_id": id,
+	})
+}
