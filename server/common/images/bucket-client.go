3f8fafc7213c97673b147d44b2ff9888cd49542b
albqvictor1508 <albq.victor@gmail.com>
2025-08-01 18:51:13 -0300

    chore: creating new bucket client

--- server/common/images/bucket-client.go
+++ server/common/images/bucket-client.go
@@ -12,14 +12,21 @@ func NewBucketClient() *s3.Client {
 	bucketName := os.Getenv("BUCKET_NAME")
 	accountId := os.Getenv("ACCOUNT_ID")
 	accessKeyId := os.Getenv("ACCESS_KEY_ID")
+	endpoint := os.Getenv("R2_ENDPOINT")
 	accessKeySecret := os.Getenv("ACCESS_KEY_SECRET")
 	region := os.Getenv("R2_REGION")
 
 	cfg := aws.Config{
 		Region:      region,
 		Credentials: aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
-	EndpointResolverWithOptions: aws.EndpointResolverWithOptionsFunc(
-			
-			)
+		EndpointResolverWithOptions: aws.EndpointResolverWithOptionsFunc(
+			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{
+					URL:               endpoint,
+					HostnameImmutable: true,
+				}, nil
+			},
+		),
 	}
+	return s3.NewFromConfig(cfg)
 }
