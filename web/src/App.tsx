6c533652d15f2bfa3d0ed790b58d4d1d57a64d3e
albqvictor1508 <albq.victor@gmail.com>
2025-07-31 23:47:56 -0300

    chore: frontend setup coming

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+export function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div>
 			<p>counter: {count}</p>
 			<p>salve</p>
+
+			<button type="button" onClick={() => setCount(count + 1)}>
+				Add to counter
+			</button>
 		</div>
 	);
 }
-
-export default App;
