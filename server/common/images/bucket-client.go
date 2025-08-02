aa01b3190fe573d2041f87c0ac881688f786a528
albqvictor1508 <albq.victor@gmail.com>
2025-08-02 11:20:23 -0300

    chore: deleting again

--- server/common/images/bucket-client.go
+++ server/common/images/bucket-client.go
@@ -1,17 +1,1 @@
 package images
-
-import (
-	"context"
-	"os"
-
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-)
-
-func NewBucketClient() *s3.Client {
-	accessKeyId := os.Getenv("ACCESS_KEY_ID")
-	accessKeySecret := os.Getenv("ACCESS_KEY_SECRET")
-	endpoint := os.Getenv("R2_ENDPOINT")
-	region := os.Getenv("R2_REGION")
-
-	ctx := context.Background()
-}
