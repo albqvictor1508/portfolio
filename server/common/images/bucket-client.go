a981bd8a3d7839325bbdd570ec4148d8f6146b4c
albqvictor1508 <albq.victor@gmail.com>
2025-08-02 11:51:14 -0300

    chore: create init, the setup func

--- server/common/images/bucket-client.go
+++ server/common/images/bucket-client.go
@@ -1,1 +1,18 @@
 package images
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var s3Session *s3.S3
+
+func init() {
+	s3Session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+	})))
+}
+
+func ListBuckets() (resp *s3.ListBucketsOutput) {
+}
