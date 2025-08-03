0dee8f94bca70912b4b51c623e61fc5001cd9666
albqvictor1508 <albq.victor@gmail.com>
2025-08-03 15:12:35 -0300

    chore: creating menu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useRef } from "react";
 
-export function App() {
-  const salve = useRef("salve");
+const client = new QueryClient();
 
-  return (
-    <div className="w-full h-full">
-      <p>{salve.current}</p>
-    </div>
-  );
+export function App() {
+	const salve = useRef("salve");
+	salve.current = "mudei o valor de salve";
+
+	return (
+		<QueryClientProvider client={client}>
+			<div className="w-full h-full">
+				<p>{salve.current}</p>
+			</div>
+		</QueryClientProvider>
+	);
 }
