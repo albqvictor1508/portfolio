3214e0c35d6ef9ada681dda10566ab5335dba592
albqvictor1508 <albq.victor@gmail.com>
2025-08-03 23:16:31 -0300

    chore: change font to martian mono

--- web/src/components/project/project.tsx
+++ web/src/components/project/project.tsx
@@ -2,7 +2,7 @@ export const Project = () => {
 	return (
 		<div className="w-full h-full flex flex-col gap-2">
 			<h2 className="font-semibold text-2xl">Zentto</h2>
-			<span>25 Jun, 2025 - Presente</span>
+			<span className="text-sm text-zinc-500">25 Jun, 2025 - Presente</span>
 			<p>lorem</p>
 
 			<div className="w-full h-60 rounded-md bg-zinc-800"></div>
--- web/src/components/project/section.tsx
+++ web/src/components/project/section.tsx
@@ -3,15 +3,25 @@ import { Project } from "./project.tsx";
 
 export const ProjectSection = ({ projects }: { projects: ProjectSchema[] }) => {
 	return (
-		<div className="w-full h-full flex flex-col gap-2">
-			<h2>Projects</h2>
-			<p>lorem</p>
+		<div className="w-full h-full flex flex-col gap-10">
+			<div className="w-full h-full flex flex-col gap-2">
+				<h2 className="text-2xl font-semibold">Projects</h2>
+				<p className="w-2/3 text-justify">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s, when an unknown printer took a galley of type and
+					scrambled it to make a type specimen book. It has survived not only
+					five centuries.
+				</p>
+			</div>
 
 			{/*
-      {projects.map(p => (
 
+      {projects.map(p => (
       ))}
     */}
+			<Project />
+			<Project />
 			<Project />
 		</div>
 	);
