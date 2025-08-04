5c7b4cdcbba64cd5d5eef9445e4ebe3130334a25
albqvictor1508 <albq.victor@gmail.com>
2025-08-04 15:50:53 -0300

    chore: remove unused things on APp

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useRef } from "react";
-import { Home } from "./components/home";
 
 const client = new QueryClient();
 
 export function App() {
-  const salve = useRef("salve");
-  salve.current = "mudei o valor de salve";
-
-  return (
-    <QueryClientProvider client={client}>
-      <Home />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}></QueryClientProvider>;
 }
