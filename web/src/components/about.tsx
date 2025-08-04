695b65aea6c037b0b8e87e42edf18bedd6cf0caa
albqvictor1508 <albq.victor@gmail.com>
2025-08-04 10:02:20 -0300

    chore: about and footer changes

--- web/src/components/about.tsx
+++ web/src/components/about.tsx
@@ -1,29 +1,33 @@
 import { Button } from "./ui/button";
 
 export const AboutSection = () => {
-	return (
-		<div className="w-full h-full flex gap-10">
-			<div className="w-full flex-1 flex flex-col gap-4">
-				<div className="w-full flex flex-col gap-2.5">
-					<h2 className="font-semibold text-2xl">About me.</h2>
-					<p className="text-sm text-justify">
-						Lorem Ipsum is simply dummy text of the printing and typesetting and
-						scrambled it to make a type specimen book. It has survived not only
-						five centuries.
-					</p>
-					<p className="text-sm text-justify">
-						with desktop publishing software like Aldus PageMaker including
-						versions of Lorem Ipsum.
-					</p>
-				</div>
-				<div className="flex gap-2">
-					<Button variant={"default"}>View on Github</Button>
-					<Button variant={"secondary"}>Contact me</Button>
-				</div>
-			</div>
-			<div className="w-full flex justify-center items-center flex-1 rounded-md border-2 border-r-zinc-800 p-8">
-				salve
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-full flex gap-10">
+      <div className="w-full flex-1 flex flex-col gap-4">
+        <div className="w-full flex flex-col gap-2.5">
+          <h2 className="font-semibold text-2xl">About me.</h2>
+          <p className="text-sm text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries.
+          </p>
+          <p className="text-sm text-justify">
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Button className="cursor-pointer" variant={"default"}>
+            View on Github
+          </Button>
+          <Button className="cursor-pointer" variant={"secondary"}>
+            Contact me
+          </Button>
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center flex-1 rounded-md border-2 border-r-zinc-800 p-8">
+        salve
+      </div>
+    </div>
+  );
 };
--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = ({ isLight }: { isLight: boolean }) => {
   const textStyle = isLight ? "text-zinc-400" : "text-zinc-800";
   return (
-    <div className="w-full flex justify-center items-center border-2 border-r-blue-600">
-      <div className="w-3/4 flex border-2 border-orange-400 p-4 ">
+    <div className="w-full flex justify-center items-center">
+      <div className="w-3/4 flex p-4 ">
         <div className="flex flex-1 flex-col gap-2">
           <h3 className="text-center w-max font-semibold text-lg">Other</h3>
           <span
