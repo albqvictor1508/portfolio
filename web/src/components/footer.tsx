2e4b9eea3ef770cbaa46beb0738dab6ef72a55aa
albqvictor1508 <albq.victor@gmail.com>
2025-08-04 08:37:09 -0300

    chore: footer ok

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,24 @@
+export const Footer = () => {
+	return (
+		<div className="w-full h-[400px] grid grid-cols-3">
+			<div className="flex flex-col gap-2">
+				<h3>Important Links</h3>
+				<span>Home</span>
+				<span>Tralalero</span>
+				<span>Tralala</span>
+			</div>
+			<div className="flex flex-col gap-2">
+				<h3>Social</h3>
+				<span>Home</span>
+				<span>Tralalero</span>
+				<span>Tralala</span>
+			</div>
+			<div className="flex flex-col gap-2">
+				<h3>Other</h3>
+				<span>Home</span>
+				<span>Tralalero</span>
+				<span>Tralala</span>
+			</div>
+		</div>
+	);
+};
