d254d1bb71d91a1331c964dd3a36fe53ba13f203
albqvictor1508 <albq.victor@gmail.com>
2025-08-04 17:31:54 -0300

    chore: still trying to create a commit table

--- web/src/components/commit-chart.tsx
+++ web/src/components/commit-chart.tsx
@@ -36,8 +36,8 @@ export const CommitChart = () => {
     return 'fill-green-950';
   };
 
-  const squareSize = 40;
-  const gap = 10;
+  const squareSize = 20;
+  const gap = 5;
   const firstDayOfWeek = days[0]?.date.getDay() ?? 0;
   const weekCount = Math.ceil((daysInPast + firstDayOfWeek) / 7);
 
