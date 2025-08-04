9f16224d679cfdb60c638521e66d6f2d23fcd256
albqvictor1508 <albq.victor@gmail.com>
2025-08-04 11:05:08 -0300

    chore: menu completed

--- web/src/components/menu.tsx
+++ web/src/components/menu.tsx
@@ -73,13 +73,13 @@ export function Menu() {
                 <NavigationMenuLink asChild>
                   <a href="#" className="flex-row items-center gap-2">
                     <CircleHelpIcon />
-                    Backlog
+                    Github
                   </a>
                 </NavigationMenuLink>
                 <NavigationMenuLink asChild>
                   <a href="#" className="flex-row items-center gap-2">
                     <CircleIcon />
-                    To Do
+                    LinkedIn
                   </a>
                 </NavigationMenuLink>
                 <NavigationMenuLink asChild>
@@ -94,25 +94,5 @@ export function Menu() {
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
-  );
-}
-
-function ListItem({
-  title,
-  children,
-  href,
-  ...props
-}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
-  return (
-    <li {...props}>
-      <NavigationMenuLink asChild>
-        <a href={href}>
-          <div className="text-sm leading-none font-medium">{title}</div>
-          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
   );
 }
