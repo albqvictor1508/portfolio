2b6d40a34b96663e76ccba75fc643a37e8782d4b
albqvictor1508 <albq.victor@gmail.com>
2025-08-05 18:58:49 -0300

    chore: creating stack section

--- web/src/components/stack.tsx
+++ web/src/components/stack.tsx
@@ -1,6 +1,17 @@
 import { Badge } from "./ui/badge";
 
 export const StackSection = () => {
+	const technologies = [
+		{ icon: "", name: "Go" },
+		{ icon: "", name: "Typescript" },
+		{ icon: "", name: "Java" },
+		{ icon: "", name: "Docker" },
+		{ icon: "", name: "AWS" },
+		{ icon: "", name: "React Native" },
+		{ icon: "", name: "Postgres" },
+		{ icon: "", name: "MongoDB" },
+		{ icon: "", name: "Websocket" },
+	];
 	return (
 		<div className="w-full h-full flex flex-col gap-4">
 			<h2 className="text-2xl font-semibold">My Technologies.</h2>
@@ -9,81 +20,18 @@ export const StackSection = () => {
 				fundamentals.
 			</p>
 			<div className="flex m-auto  gap-4 items-center flex-wrap justify-center ">
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
-				<Badge variant={"outline"} className="text-sm">
-					salve
-				</Badge>
+				{technologies.map((tech: { icon: string; name: string }) => (
+					<Badge
+						variant={"outline"}
+						className="p-2 flex justify-center items-center gap-2"
+						key={tech.name}
+					>
+						<span className="w-6 h-6">
+							<img src={tech.icon} alt="tech icon" />
+						</span>
+						{tech.name}
+					</Badge>
+				))}
 			</div>
 		</div>
 	);
