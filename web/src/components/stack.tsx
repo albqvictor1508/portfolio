d6cb7f927e3f5b763e5dad4269acfcc52a02a5dd
albqvictor1508 <albq.victor@gmail.com>
2025-08-05 11:40:35 -0300

    chore: creating badge for stack

--- web/src/components/stack.tsx
+++ web/src/components/stack.tsx
@@ -2,9 +2,12 @@ import { Badge } from "./ui/badge";
 
 export const StackSection = () => {
 	return (
-		<div className="w-full h-full">
-			<h2>My Technologies.</h2>
-			<p>lorem</p>
+		<div className="w-full h-full flex flex-col gap-4">
+			<h2 className="text-2xl font-semibold">My Technologies.</h2>
+			<p className="text-sm text-zinc-400">
+				Building high-performance websites with clean code and strong SEO
+				fundamentals.
+			</p>
 			<div className="flex m-auto  gap-4 items-center flex-wrap justify-center ">
 				<Badge variant={"outline"} className="text-sm">
 					salve
