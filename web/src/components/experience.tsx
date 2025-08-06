9c595de6cd9e52ae16160b9ede2c28d13c12de47
albqvictor1508 <albq.victor@gmail.com>
2025-08-06 17:33:15 -0300

    wip: experience section initial commit

---
+++ web/src/components/experience.tsx
@@ -0,0 +1,10 @@
+export const ExperienceSection = () => {
+  const experiences = {
+    companyName: "salve",
+    startDate: new Date(),
+    endDate: null,
+    role: "backend",
+    nsei: "salve",
+  };
+  return <div className="w-full h-full flex flex-col gap-4 "></div>;
+};
