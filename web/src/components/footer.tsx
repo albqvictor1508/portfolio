72d629e1a61a0569b39a20490d3a7f8e7f0934b7
albqvictor1508 <albq.victor@gmail.com>
2025-08-06 10:26:13 -0300

    chore: end-2-end to validate footer layout

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,68 +1,68 @@
 export const Footer = ({ isLight }: { isLight: boolean }) => {
-  const textStyle = isLight ? "text-zinc-400" : "text-zinc-800";
-  return (
-    <div
-      id="footer"
-      className="w-2/3 border-2 border-rose-500 flex justify-center items-center"
-    >
-      <div className="w-full flex p-4 border-2 border-blue-500 justify-center">
-        <div className="flex flex-1 flex-col justify-center gap-2">
-          <h3 className="w-fit font-semibold text-base border border-green-400">
-            Important Links
-          </h3>
-          <span
-            className={`${textStyle} text-sm w-fit hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max border border-red-500 hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-        </div>
-        <div className="flex flex-1 flex-col gap-2 justify-center">
-          <h3 className="w-max font-semibold text-base">Other</h3>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-        </div>
-        <div className="flex flex-1 flex-col gap-2 justify-center">
-          <h3 className="w-max font-semibold text-base">Other</h3>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-          <span
-            className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
-          >
-            Home
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+	const textStyle = isLight ? "text-zinc-400" : "text-zinc-800";
+	return (
+		<div
+			id="footer"
+			className="w-2/3 flex justify-center items-center"
+		>
+			<div className="w-full flex p-4 justify-center">
+				<div className="flex flex-1 flex-col justify-center gap-2 border border-green-400">
+					<h3 className="w-fit font-semibold text-base">
+						Important Links
+					</h3>
+					<span
+						className={`${textStyle} text-sm w-fit hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+				</div>
+				<div className="flex flex-1 flex-col gap-2 justify-center border border-purple-500">
+					<h3 className="w-max font-semibold text-base">Other</h3>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+				</div>
+				<div className="flex flex-1 flex-col gap-2 justify-center border border-orange-500">
+					<h3 className="w-max font-semibold text-base">Other</h3>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+					<span
+						className={`${textStyle} text-sm w-max hover:underline cursor-pointer`}
+					>
+						Home
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
