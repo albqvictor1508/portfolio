40a1e174433f4a095bdfcd67bce816dffc95cbc4
albqvictor1508 <albq.victor@gmail.com>
2025-08-08 23:33:19 -0300

    chore: add my cv and changes on cv.go

--- server/routes/cv.go
+++ server/routes/cv.go
@@ -1,9 +1,25 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SendCV(ctx *gin.Context) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println("Error getting working directory:", err)
+		ctx.AbortWithStatus(500)
+		return
+	}
+
+	pdfPath := filepath.Join(wd, "public", "VICTOR_ALBUQUERQUE_CV.pdf")
+	log.Println("Attempting to serve PDF from:", pdfPath)
+
 	ctx.Header("Content-Type", "application/pdf")
 	ctx.Header("Content-Disposition", "attachment; filename=VICTOR_ALBUQUERQUE_CV.pdf")
-	ctx.File("../public/cv.pdf")
+	ctx.File(pdfPath)
 }
