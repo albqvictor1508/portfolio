beadc157b1db08544b528ea5f075eee5bcd68c68
albqvictor1508 <albq.victor@gmail.com>
2025-08-09 00:19:14 -0300

    chore: add typing effect on hero

--- web/src/components/typing-effect.tsx
+++ web/src/components/typing-effect.tsx
@@ -1,36 +1,19 @@
-import React, { useEffect, useState } from "react";
+import TypeWriter from "typewriter-effect";
 
-interface TypingEffectProps {
-	text: string;
-	speed?: number;
-}
-
-const TypingEffect: React.FC<TypingEffectProps> = ({ text, speed = 150 }) => {
-	const [displayedText, setDisplayedText] = useState("");
-	const [index, setIndex] = useState(0);
-	const [showCursor, setShowCursor] = useState(true);
-
-	useEffect(() => {
-		if (index < text.length) {
-			const timeoutId = setTimeout(() => {
-				setDisplayedText((prev) => prev + text.charAt(index));
-				setIndex((prev) => prev + 1);
-			}, speed);
-			return (): void => clearTimeout(timeoutId);
-		}
-
-		const cursorInterval = setInterval(() => {
-			setShowCursor((prev) => !prev);
-		}, 500);
-		return (): void => clearInterval(cursorInterval);
-	}, [text, index, speed]);
-
-	return (
-		<span>
-			{displayedText}
-			<span className={showCursor ? "typing-cursor" : ""}>&nbsp;</span>
-		</span>
-	);
+type TypingEffectProps = {
+  text: string;
 };
 
-export default TypingEffect;
+export const TypingEffect = ({ text }: TypingEffectProps) => {
+  return (
+    <div>
+      <TypeWriter
+        options={{
+          strings: [text, text, text, text, text, text],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+    </div>
+  );
+};
