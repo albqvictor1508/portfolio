229c5408f59bfed087f3492dfe4903982912409d
albqvictor1508 <albq.victor@gmail.com>
2025-08-12 08:17:25 -0300

    chore: nc deleted

--- server/start.sh
+++ server/start.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -ex
 
-# Wait for PostgreSQL to be ready
-# echo "Waiting for PostgreSQL to be ready..."
-# until nc -z localhost 6008; do
-# echo "PostgreSQL is unavailable - sleeping"
-# sleep 1
-# done
-# echo "PostgreSQL is up - executing migrations"
-
 # Run migrations using the compiled goose binary
 /app/goose -dir /app/migrations postgres "$DATABASE_URL" up
 
