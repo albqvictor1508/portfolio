0e08d05f9e722562be5efb1aed8a86d8b977ef14
albqvictor1508 <albq.victor@gmail.com>
2025-08-12 00:00:59 -0300

    chore: comment start.sh connection

--- server/start.sh
+++ server/start.sh
@@ -2,12 +2,12 @@
 set -ex
 
 # Wait for PostgreSQL to be ready
-echo "Waiting for PostgreSQL to be ready..."
-until nc -z localhost 6008; do
-  echo "PostgreSQL is unavailable - sleeping"
-  sleep 1
-done
-echo "PostgreSQL is up - executing migrations"
+# echo "Waiting for PostgreSQL to be ready..."
+# until nc -z localhost 6008; do
+# echo "PostgreSQL is unavailable - sleeping"
+# sleep 1
+# done
+# echo "PostgreSQL is up - executing migrations"
 
 # Run migrations using the compiled goose binary
 /app/goose -dir /app/migrations postgres "$DATABASE_URL" up
@@ -15,4 +15,3 @@ echo "PostgreSQL is up - executing migrations"
 # Start the API server
 echo "Starting API server"
 ./server
-
