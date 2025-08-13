e811041e07f0a37c0016d6f4ec806dc0cc58bd12
albqvictor1508 <albq.victor@gmail.com>
2025-08-13 10:58:58 -0300

    chore: change change lang btn

--- web/src/components/menu.tsx
+++ web/src/components/menu.tsx
@@ -1,117 +1,119 @@
 import { Link } from "@tanstack/react-router";
 
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { useLanguage } from "../context/LanguageContext";
 import { Button } from "./ui/button";
 
 export function Menu() {
-  const { setLanguage, t } = useLanguage();
+	const { setLanguage, t } = useLanguage();
 
-  return (
-    <div className="w-full mt-10 flex justify-between items-center">
-      <div className="flex items-center gap-4">
-        <Link to=".." className="font-bold">
-          VA
-        </Link>
-        <NavigationMenu viewport={false}>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/">{t("menu.home")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+	return (
+		<div className="w-full mt-10 flex justify-between items-center">
+			<div className="flex items-center gap-4">
+				<Link to=".." className="font-bold">
+					VA
+				</Link>
+				<NavigationMenu viewport={false}>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/">{t("menu.home")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/#about">{t("menu.about")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/#about">{t("menu.about")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/#projects">{t("menu.projects")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/#projects">{t("menu.projects")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/#experience">{t("menu.experience")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/#experience">{t("menu.experience")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/#stack">{t("menu.stack")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/#stack">{t("menu.stack")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <a href="/#contact">{t("menu.contact")}</a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-      <NavigationMenu viewport={false}>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="text-white">
-              🌐
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[100px] gap-1 p-2">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Button
-                      className="flex items-center gap-2"
-                      onClick={() => setLanguage("pt")}
-                    >
-                      🇧🇷 PT
-                    </Button>
-                  </NavigationMenuLink>
-                </li>
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Button
-                      className="flex items-center gap-2"
-                      onClick={() => setLanguage("en")}
-                    >
-                      🇺🇸 EN
-                    </Button>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  );
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								asChild
+								className={navigationMenuTriggerStyle()}
+							>
+								<a href="/#contact">{t("menu.contact")}</a>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+			<NavigationMenu viewport={false}>
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<NavigationMenuTrigger className="text-white">
+							🌐
+						</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							<ul className="grid w-[100px] gap-1 p-2">
+								<li>
+									<NavigationMenuLink asChild>
+										<Button
+											variant={"ghost"}
+											className="flex items-center gap-2"
+											onClick={() => setLanguage("pt")}
+										>
+											🇧🇷 PT
+										</Button>
+									</NavigationMenuLink>
+								</li>
+								<li>
+									<NavigationMenuLink asChild>
+										<Button
+											variant={"ghost"}
+											className="flex items-center gap-2"
+											onClick={() => setLanguage("en")}
+										>
+											🇺🇸 EN
+										</Button>
+									</NavigationMenuLink>
+								</li>
+							</ul>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</div>
+	);
 }
