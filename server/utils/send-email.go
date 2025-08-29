8c1433d265ecc1ea962ed0643b78aa0260955a38
albqvictor1508 <albq.victor@gmail.com>
2025-08-29 15:32:14 -0300

    fix: validate reply-to and correct smtp port

--- server/utils/send-email.go
+++ server/utils/send-email.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 
 	gomail "gopkg.in/mail.v2"
@@ -24,16 +25,21 @@ func SendEmail(params SendEmailParams) error {
 		params.Subject = "By Portfolio"
 	}
 
+	if params.ReplyTo == "" {
+		return fmt.Errorf("ReplyTo is required")
+	}
+
 	message.SetHeader("From", myEmail)
 	// message.SetHeader("Reply-To", params.ReplyTo)
 	message.SetHeader("To", myEmail)
 	message.SetHeader("Subject", params.Subject)
 	message.SetBody("text/plain", params.Content)
 
-	dialer := gomail.NewDialer("smtp.gmail.com", 465, myEmail, myPassword)
+	dialer := gomail.NewDialer("smtp.gmail.com", 567, myEmail, myPassword)
 
 	if err := dialer.DialAndSend(message); err != nil {
 		return err
 	}
+
 	return nil
 }
